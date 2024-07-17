fd2a8db2fa52095e8820637a1dd945688be6dd2f
Amit Kulkarni <154905823+Amit-Webdev@users.noreply.github.com>
2024-07-17 21:50:45 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f43a279d-f305-4741-84f5-ddbea479b7c2';
+export const MODAL_CLOSE_SEC = 2.5;
